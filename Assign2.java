6c71e51dd5fac99c89e02caa29d03ea858b73147
lesinakbar <lesinakbar@gmail.com>
2016-07-25 20:54:58 +0530

    Create Assign2.java

---
+++ Assign2.java
@@ -0,0 +1,12 @@
+/* Date : 22/07/2016
+ * Declare three integer variables (a,b,c) in a single declaration and simultaneously initialize b to 4
+ */
+public class Assgn2 {
+
+	public static void main(String[] args) {
+		
+		int a,b=4,c; // Required initialization done.
+
+	}
+
+}
